18ea068049892148346a824ce4b2fe15f189d03b
ebsamson3 <ebsamson3@gmail.com>
2019-10-23 12:58:40 -0500

    Removed initial view controller from storyboard.

--- Example/SwiftOverpassAPI/AppDelegate.swift
+++ Example/SwiftOverpassAPI/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+		window = UIWindow(frame: UIScreen.main.bounds)
 		window?.makeKeyAndVisible()
 		let navigationController = UINavigationController()
 		navigationCoordinator = OverpassDemoCoordinator(
